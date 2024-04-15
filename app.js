7261ce00a4eb59064f9ee4d638ffceb64b45d7e0
DanielLegatesJunior <126569116+DanielLegatesJunior@users.noreply.github.com>
2024-04-15 12:04:01 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+function verificarIdade(idade) {
+    if (idade >= 18) {
+        console.log("O usuário é maior de idade.");
+    } else {
+        console.log("O usuário é menor de idade.");
+    }
+}
+
+// Exemplo de uso da função:
+verificarIdade(20); // Saída: O usuário é maior de idade.
+verificarIdade(16); // Saída: O usuário é menor de idade.
